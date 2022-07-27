a144b31847a94d936c7a33b03d08435429f3b3c8
unknown <pankajshinde798@gmail.com>
2022-07-27 17:09:38 +0530

    first commit

---
+++ src/components/Nav.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import "./Nav.css";
+
+function Nav() {
+  const [show, handleshow] = useState(false)
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 150) {
+        handleshow(true)
+      } else handleshow(false)
+    })
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav_black"}`}>
+      <img
+        className="nav_logo"
+        src="https://th.bing.com/th/id/R.4d30a09d38e6dfe3feaa31920a680108?rik=GnWkgmKvj5nwNg&riu=http%3a%2f%2fwww.cultjer.com%2fimg%2fug_photo%2f2016_09%2f76248820160921034356.jpg&ehk=grD%2fdqb%2b43FMvO3KHmAdPwgUvOikuZkwrJQwMPdCHUM%3d&risl=&pid=ImgRaw&r=0"
+        alt="Netflix Logo"
+      />
+      <img
+        className="nav_avatar"
+        src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/366be133850498.56ba69ac36858.png"
+        alt="Netflix Logo"
+      />
+    </div>
+  )
+}
+
+export default Nav
